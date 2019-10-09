cc6a1bddfd89aba6392ba5a9c8d728f37e07e53b
刘斌IT801918 <liub801918@odc.cmbchina.cn>
2019-10-09 16:13:43 +0800

    liubin:调整

--- src/com/fish/bin/api/HttpUtils.java
+++ src/com/fish/bin/api/HttpUtils.java
@@ -17,18 +17,10 @@
 package com.fish.bin.api;
 
 import com.intellij.openapi.util.io.StreamUtil;
-import org.apache.http.Header;
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-
-import java.util.List;
 
 /**
  * Created by Wesley Lin on 12/2/14.
